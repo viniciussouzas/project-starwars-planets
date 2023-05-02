4e7bfc4f0fcdcb2ac4d25ad5afe4138d1f1f370a
Vinícius Souza <tradutorvinicius@gmail.com>
2023-05-02 16:55:29 -0300

    Resolve requisito 2

--- src/components /Table.js
+++ src/components /Table.js
@@ -1,11 +1,30 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import TableContext from '../contexts /TableContext';
 
 function Table() {
   const { data } = useContext(TableContext);
 
+  const [inputName, setInputName] = useState('');
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    switch (name) {
+    case 'inputName':
+      return setInputName(value);
+    default:
+    }
+  };
+
   return (
     <main>
+      <input
+        name="inputName"
+        type="text"
+        value={ inputName }
+        onChange={ handleChange }
+        data-testid="name-filter"
+      />
       {data !== null && (
         <table>
           <thead>
@@ -26,23 +45,24 @@ function Table() {
             </tr>
           </thead>
           <tbody>
-            { data && data.map((item) => (
-              <tr key={ item.orbital_period }>
-                <td>{item.name}</td>
-                <td>{item.rotation_period}</td>
-                <td>{item.orbital_period}</td>
-                <td>{item.diameter}</td>
-                <td>{item.climate}</td>
-                <td>{item.gravity}</td>
-                <td>{item.terrain}</td>
-                <td>{item.surface_water}</td>
-                <td>{item.population}</td>
-                <td>{item.films.map((film, index) => <p key={ index }>{film}</p>)}</td>
-                <td>{item.created}</td>
-                <td>{item.edited}</td>
-                <td>{item.url}</td>
-              </tr>
-            ))}
+            { data && data.filter((item) => item.name.toLowerCase().includes(inputName))
+              .map((item) => (
+                <tr key={ item.orbital_period }>
+                  <td>{item.name}</td>
+                  <td>{item.rotation_period}</td>
+                  <td>{item.orbital_period}</td>
+                  <td>{item.diameter}</td>
+                  <td>{item.climate}</td>
+                  <td>{item.gravity}</td>
+                  <td>{item.terrain}</td>
+                  <td>{item.surface_water}</td>
+                  <td>{item.population}</td>
+                  <td>{item.films.map((film, index) => <p key={ index }>{film}</p>)}</td>
+                  <td>{item.created}</td>
+                  <td>{item.edited}</td>
+                  <td>{item.url}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       )}
